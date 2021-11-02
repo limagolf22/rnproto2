734e0098502a637595bb794c6218aba8cef4221c
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-11-02 01:52:30 +0100

    first commit + wind sign

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { FunctionComponent } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Svg, { Circle, Line } from "react-native-svg";
+import BaseExample from "./src/Wind";
+import Wind from "./src/Wind";
+//import Wind from './src/Wind';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Wind speed={65} heading={10} />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/Wind.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Component, FunctionComponent } from "react";
+import Svg, { Line, NumberProp, Polygon } from "react-native-svg";
+
+export default class Wind extends Component<{
+  speed: number;
+  heading: number;
+}> {
+  speed: Float32Array;
+  heading: Float32Array;
+  compt50: number;
+  compt10: number;
+  compt5: number;
+
+  constructor(props: any) {
+    super(props);
+    this.speed = props.speed;
+    this.heading = props.heading;
+    this.compt50 = 0;
+    this.compt10 = 0;
+    this.compt5 = 0;
+  }
+  render() {
+    this.updateCompt();
+    return (
+      <Svg
+        rotation={this.props.heading.valueOf()}
+        viewBox="0 0 100 100"
+        width="100%"
+        height="300"
+      >
+        <Line
+          x1="50"
+          x2="50"
+          y1={this.compt50 == 0 && this.compt10 > 0 ? "10" : "0"}
+          y2="100"
+          stroke="black"
+          strokeWidth="1.5"
+        />
+        {this.compt50 > 0 ? (
+          <Polygon points="50,0 75,5 50,10" fill="black" stroke="black" />
+        ) : null}
+
+        {[...Array(this.compt10)].map((x, i) => (
+          <Line
+            x1="50"
+            y1={10 + this.compt50 * 5 + i * 5}
+            x2="75"
+            y2={5 + this.compt50 * 5 + i * 5}
+            stroke="black"
+            strokeWidth="1"
+          />
+        ))}
+
+        {[...Array(this.compt5)].map((x, i) => (
+          <Line
+            x1="50"
+            y1={10 + this.compt50 * 5 + this.compt10 * 5 + i * 5}
+            x2="62.5"
+            y2={7.5 + this.compt50 * 5 + this.compt10 * 5 + i * 5}
+            stroke="black"
+            strokeWidth="1"
+          />
+        ))}
+      </Svg>
+    );
+  }
+
+  updateCompt() {
+    this.compt50 = Math.floor(this.props.speed / 50);
+    this.compt10 = Math.floor((this.props.speed - this.compt50 * 50) / 10);
+    this.compt5 = Math.floor(
+      (this.props.speed - this.compt50 * 50 - this.compt10 * 10) / 5
+    );
+  }
+}
